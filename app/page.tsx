301b3eaaec53cfdd58d0819709588a76e70b3d16
odetv <abdiutama02@gmail.com>
2025-11-28 08:15:22 +0800

    feat: adding preview location and button update location

--- app/page.tsx
+++ app/page.tsx
@@ -157,20 +157,50 @@ export default function Home() {
             placeholder="Uraian Kegiatan"
             value={kegiatan}
             onChange={(e) => setKegiatan(e.target.value)}
-            rows={6}
+            rows={3}
             className="w-full px-3 py-2 border rounded-lg resize-y"
           ></textarea>
 
-          <div className="text-sm text-gray-600">
-            Lokasi:{" "}
-            {latitude && longitude ? (
-              <span className="font-medium">
-                {latitude}, {longitude}
-              </span>
-            ) : (
-              <span className="text-red-600">
-                Tidak dapat menemukan lokasi. Pastikan GPS aktif.
-              </span>
+          <div className="text-sm text-gray-600 space-y-2">
+            <div className="flex items-center justify-between gap-2">
+              <div>
+                Lokasi:{" "}
+                {latitude && longitude ? (
+                  <span className="font-medium">
+                    {latitude}, {longitude}
+                  </span>
+                ) : (
+                  <span className="text-red-600">
+                    Tidak dapat menemukan lokasi. Pastikan GPS aktif.
+                  </span>
+                )}
+              </div>
+
+              <button
+                onClick={() => {
+                  navigator.geolocation.getCurrentPosition((pos) => {
+                    setLatitude(pos.coords.latitude.toFixed(6));
+                    setLongitude(pos.coords.longitude.toFixed(6));
+                  });
+                }}
+                className="text-blue-500 font-semibold hover:underline"
+              >
+                Perbarui Lokasi
+              </button>
+            </div>
+
+            {latitude && longitude && (
+              <iframe
+                title="Google Maps Interactive"
+                width="100%"
+                height="250"
+                className="rounded-md border"
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                src={`https://maps.google.com/maps?&q=${latitude},${longitude}&z=17&output=embed`}
+              ></iframe>
             )}
           </div>
 
