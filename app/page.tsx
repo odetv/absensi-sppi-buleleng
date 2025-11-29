0cddd7aa4140f362672e39647747deaef35c44eb
odetv <abdiutama02@gmail.com>
2025-11-29 21:09:09 +0800

    chore: adding marker maps geolocation sppg using mymaps

--- app/page.tsx
+++ app/page.tsx
@@ -14,29 +14,35 @@ export default function Home() {
           <p className="text-sm">Daftar Absensi Harian SPPI BGN</p>
           <LogoutButton />
         </div>
-        <div className="flex flex-row justify-center items-center gap-2">
+        <div className="flex flex-row justify-center items-center gap-2 w-full">
           <Link
-            className="px-6 py-3 bg-blue-500 rounded-lg text-white font-semibold text-center hover:bg-blue-600 transition-colors"
+            className="px-5 py-2 bg-blue-500 rounded-lg text-white font-semibold text-center hover:bg-blue-600 transition-colors"
             href="bgn-pusat"
             rel="noopener noreferrer"
           >
             BGN Pusat
           </Link>
           <Link
-            className="px-6 py-3 bg-blue-500 rounded-lg text-white font-semibold text-center hover:bg-blue-600 transition-colors"
+            className="px-5 py-2 bg-blue-500 rounded-lg text-white font-semibold text-center hover:bg-blue-600 transition-colors"
             href="kppg-denpasar"
             rel="noopener noreferrer"
           >
             KPPG Denpasar
           </Link>
           <Link
-            className="px-6 py-3 bg-blue-500 rounded-lg text-white font-semibold text-center hover:bg-blue-600 transition-colors"
+            className="px-5 py-2 bg-blue-500 rounded-lg text-white font-semibold text-center hover:bg-blue-600 transition-colors"
             href="korwil-buleleng"
             rel="noopener noreferrer"
           >
             Korwil Buleleng
           </Link>
         </div>
+        <iframe
+          className="mt-2 rounded-lg"
+          src="https://www.google.com/maps/d/u/0/embed?mid=1loSa0QJm5MZ0QctKbvVr8RkQyc11L6s&ehbc=2E312F&noprof=1"
+          width="100%"
+          height="340"
+        ></iframe>
         <Footer />
       </div>
     </main>
