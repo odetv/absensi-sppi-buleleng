8134c39f097f33da06076f4cfe25878b11a0f1e1
odetv <abdiutama02@gmail.com>
2025-12-28 21:55:43 +0800

    feat: update middleware delete maintenance checker

--- middleware.ts
+++ middleware.ts
@@ -4,50 +4,50 @@ import { jwtVerify } from "jose";
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  /** =========================
-   * 1. BYPASS STATIC & API CHECK
-   ========================= */
-  if (
-    pathname.startsWith("/api/googleapis/general/maintenance") ||
-    pathname.startsWith("/_next") ||
-    pathname.includes(".")
-  ) {
-    return NextResponse.next();
-  }
+  //   /** =========================
+  //    * 1. BYPASS STATIC & API CHECK
+  //    ========================= */
+  //   if (
+  //     pathname.startsWith("/api/googleapis/general/maintenance") ||
+  //     pathname.startsWith("/_next") ||
+  //     pathname.includes(".")
+  //   ) {
+  //     return NextResponse.next();
+  //   }
 
-  /** =========================
-   * 2. CEK MAINTENANCE
-   ========================= */
-  let maintenance = false;
-  try {
-    const protocol = request.headers.get("x-forwarded-proto") ?? "http";
-    const host = request.headers.get("host");
-    const res = await fetch(
-      `${protocol}://${host}/api/googleapis/general/maintenance`,
-      { cache: "no-store" }
-    );
-    const data = await res.json();
-    maintenance = Boolean(data.maintenance);
-  } catch (err) {
-    console.error("Maintenance check failed", err);
-  }
+  //   /** =========================
+  //    * 2. CEK MAINTENANCE
+  //    ========================= */
+  //   let maintenance = false;
+  //   try {
+  //     const protocol = request.headers.get("x-forwarded-proto") ?? "http";
+  //     const host = request.headers.get("host");
+  //     const res = await fetch(
+  //       `${protocol}://${host}/api/googleapis/general/maintenance`,
+  //       { cache: "no-store" }
+  //     );
+  //     const data = await res.json();
+  //     maintenance = Boolean(data.maintenance);
+  //   } catch (err) {
+  //     console.error("Maintenance check failed", err);
+  //   }
 
-  /** =========================
- * 3. RULE MAINTENANCE (ANTI LOOP)
- ========================= */
-  // 🔴 MAINTENANCE AKTIF
-  if (maintenance) {
-    // Kalau sudah di /maintenance → LANJUT, JANGAN REDIRECT
-    if (pathname === "/maintenance") {
-      return NextResponse.next();
-    }
-    // Selain itu → redirect ke /maintenance
-    return NextResponse.redirect(new URL("/maintenance", request.url));
-  }
-  // 🟢 MAINTENANCE MATI
-  if (!maintenance && pathname === "/maintenance") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  //   /** =========================
+  //  * 3. RULE MAINTENANCE (ANTI LOOP)
+  //  ========================= */
+  //   // 🔴 MAINTENANCE AKTIF
+  //   if (maintenance) {
+  //     // Kalau sudah di /maintenance → LANJUT, JANGAN REDIRECT
+  //     if (pathname === "/maintenance") {
+  //       return NextResponse.next();
+  //     }
+  //     // Selain itu → redirect ke /maintenance
+  //     return NextResponse.redirect(new URL("/maintenance", request.url));
+  //   }
+  //   // 🟢 MAINTENANCE MATI
+  //   if (!maintenance && pathname === "/maintenance") {
+  //     return NextResponse.redirect(new URL("/", request.url));
+  //   }
 
   /** =========================
    * 4. AUTH CHECK
